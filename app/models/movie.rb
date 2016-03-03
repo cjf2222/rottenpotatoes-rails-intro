296974d9c869f090489a6b48269787f1acefb6c0
cjf2222 <connorflatt@gmail.com>
2016-03-02 22:56:01 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,12 @@
 class Movie < ActiveRecord::Base
-  def self.movies(filters, sort_field)
-    self.where({:rating => filters}).order(sort_field)
-  end
+ 
   
   def self.ratings
     self.pluck(:rating).uniq
+  end
+  
+   def self.movies(filters, sort_field)
+    self.where({:rating => filters}).order(sort_field)
   end
   
 end
