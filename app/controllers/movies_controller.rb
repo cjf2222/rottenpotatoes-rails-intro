5e8f6a8848b93e8ae385a9bf29c757c5031933a9
cjf2222 <connorflatt@gmail.com>
2016-03-02 22:34:48 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,7 +19,7 @@ class MoviesController < ApplicationController
    
   def index
     @movies = Movie.order(params[:order_by])
-    @ratings =Movie.ratings
+    @ratings =Movie.pluck(:rating).uniq
   end
 
   def new
