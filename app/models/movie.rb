0700e442366732fd92cbddfad5484683bc2c00be
cjf2222 <connorflatt@gmail.com>
2016-03-02 23:35:58 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
   end
   
   def self.movies(filters, sort_field)
-    self.where(:rating => filters).order(sort_field)
+    self.where({:rating => filters}).order(sort_field)
   end
   
 end
