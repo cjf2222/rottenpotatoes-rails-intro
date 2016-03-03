d5dd19ecc5719ffa350fe7d73bd98b1a0f248dfd
cjf2222 <connorflatt@gmail.com>
2016-03-03 01:44:37 -0600

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -17,26 +17,10 @@ class MoviesController < ApplicationController
     @rate = @ratings
     if(params[:ratings].present?)
       @rate = params[:ratings].keys
-      session[:ratings] =@rate
-    else
-        if(session[:ratings].present?)
-        @rate = session[:ratings].keys
-      else
-        session[:ratings] = @rate
-        end
-    end
-    
-    @sort = " "
-    if(params[:sort].present?)
-       @sort = params[:sort]
-       session[:sort] =@sort
-    else
-       if(session[:sort].present?)
-        @sort = session[:sort]
-       end
-    end
+     
+ 
 
-    @movies = Movie.where(rating: @rate).order(@sort)                    #movies(params[:ratings].keys, params[:order_by])
+    @movies = Movie.where(rating: @rate).order(params[:sort])                    #movies(params[:ratings].keys, params[:order_by])
   
  # session[:sort] =@sort
  # session[:ratings] =@rate
