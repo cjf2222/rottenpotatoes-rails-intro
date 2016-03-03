453b6d8fd4f4d4e1ac8f4e2afa1290c13584da07
cjf2222 <connorflatt@gmail.com>
2016-03-02 23:49:27 -0600

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,7 +5,7 @@ class Movie < ActiveRecord::Base
   end
   
   def self.movies(filters, sort_field)
-    self.order(sort_field) if not filters
+    return self.order(sort_field) if not filters
     self.where({:rating => filters.keys}).order(sort_field)
   end
   
